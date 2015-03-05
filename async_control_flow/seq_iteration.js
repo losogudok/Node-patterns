3e2f1aeb03721c2d298cd704f9937a9ef125b6d3
Andrey Kostin <losogudok@yandex.ru>
2015-03-05 10:38:40 +0300

    parall

--- async_control_flow/seq_iteration.js
+++ async_control_flow/seq_iteration.js
@@ -1,4 +1,4 @@
-iterateSeries(collection, iteratorCallback, finalCallback) {
+function iterateSeries(collection, iteratorCallback, finalCallback) {
     function iterate(index) {
         if (index === collection.length) {
             return finalCallback();
@@ -6,11 +6,33 @@ iterateSeries(collection, iteratorCallback, finalCallback) {
         iteratorCallback(collection[index], function(err){
             if (err) {
                 return finalCallback(err);
-            }       
+            }
             iterate(index + 1);
         });
     }
     iterate(0);
 }
 
+function iterateParallel(collection, iterator, final) {
+  var completed = 0;
+
+  function done(err) {
+      if (err) {
+          return final(err);
+      }
+      else {
+        completed++;
+        if (completed === collection.length) {
+          final();
+        }
+      }
+  }
+
+  collection.forEach(function(item){
+      iterator(item, done)
+  });
+}
+
+
 module.exports.iterateSeries = iterateSeries;
+module.exports.iterateParallel = iterateParallel;
